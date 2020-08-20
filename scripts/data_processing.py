807a7b45c366d3f6b77ebaa7b97fe6ae29a8fe83
alexei <bitskoff@gmail.com>
2020-08-20 11:29:06 +0800

    fixed_x

--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -40,9 +40,11 @@ def timestamp_to_features(timestamps):
     x = []
     for t in timestamps:
         date = datetime.datetime.fromtimestamp(t)
-        doy = np.sin(date.timetuple().tm_yday * 2 * np.pi / 365)
-        tod = np.sin((date.hour + date.minute * 60) * 2 * np.pi / 24 / 60)
-        x.append([doy, tod])
+        sdoy = np.sin(date.timetuple().tm_yday * 2 * np.pi / 365)
+        stod = np.sin((date.hour + date.minute * 60) * 2 * np.pi / 24 / 60)
+        cdoy = np.cos(date.timetuple().tm_yday * 2 * np.pi / 365)
+        ctod = np.cos((date.hour + date.minute * 60) * 2 * np.pi / 24 / 60)
+        x.append([sdoy, cdoy, stod, ctod])
     return x
 
 
